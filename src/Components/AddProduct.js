cc295ffceb10c95922ff772387a338e3b5f0ceb7
aanupam2004-source <anupam232322@arkajainuniversity.ac.in>
2026-07-06 17:43:06 +0530

    addproduct

--- src/Components/AddProduct.js
+++ src/Components/AddProduct.js
@@ -22,18 +22,7 @@ const SignupSchema = Yup.object({
 
   category: Yup.string().required("Select category"),
 
-<<<<<<< Updated upstream
-  foodName: Yup.string()
-  .matches(
-    /^[a-zA-Z\s]+$/,
-    "Food name can contain only letters and spaces"
-  )
-  .min(2, "Minimum 2 characters")
-  .max(50, "Maximum 50 characters")
-  .required("Food name is required"),
-=======
   foodName: Yup.string().min(2).max(50).required("Food name is required"),
->>>>>>> Stashed changes
 
   price: Yup.number()
     .typeError("Price must be a number")
@@ -71,24 +60,6 @@ export default function AddProduct() {
   return (
     <div>
       <Container>
-<<<<<<< Updated upstream
-
-        <Row>
-          <Col md={5}>
-            <Sidebar />
-          </Col>
-          <Col md={7}>
-            <h1>Add products</h1>
-          </Col>
-          <Breadcrumb>
-            <Breadcrumb.Item > <Link to={'/Dashboard'}>Dashboard</Link></Breadcrumb.Item>
-            <Breadcrumb.Item active>Add Products</Breadcrumb.Item>
-          </Breadcrumb>
-        </Row>
-      </Container>
-
-
-=======
         <Row>
           <Col md={1}>
             <Sidebar />
@@ -105,7 +76,7 @@ export default function AddProduct() {
         </Row>
       </Container>
 
->>>>>>> Stashed changes
+
       <Formik
         initialValues={{
           foodType: "Veg",
@@ -122,10 +93,6 @@ export default function AddProduct() {
           resetForm();
         }}
       >
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
         {({ values, setFieldValue }) => (
           <Form
             style={{
@@ -133,155 +100,6 @@ export default function AddProduct() {
               margin: "30px auto",
             }}
           >
-<<<<<<< Updated upstream
-
-
-            {/* Food Type */}
-
-            <label>Food Type</label>
-
-            <Field
-              as="select"
-              name="foodType"
-              className="form-control"
-              onChange={(e) => {
-                const value = e.target.value;
-                setFieldValue("foodType", value);
-                setFieldValue("category", categories[value][0]);
-                setSubCategories(categories[value]);
-              }}
-            >
-              <option value="Veg">Veg</option>
-              <option value="Non-Veg">Non-Veg</option>
-            </Field>
-
-            <ErrorMessage
-              name="foodType"
-              component="div"
-              className="text-danger"
-            />
-
-            <br />
-
-            {/* Category */}
-
-            <label>Category</label>
-
-            <Field as="select" name="category" className="form-control">
-              {subCategories.map((cat) => (
-                <option key={cat}>{cat}</option>
-              ))}
-            </Field>
-
-            <ErrorMessage
-              name="category"
-              component="div"
-              className="text-danger"
-            />
-
-            <br />
-
-            {/* Food Name */}
-
-            <label>Food Name</label>
-
-            <Field
-              name="foodName"
-              type="text"
-              className="form-control"
-            />
-
-            <ErrorMessage
-              name="foodName"
-              component="div"
-              className="text-danger"
-            />
-
-            <br />
-
-            {/* Price */}
-
-            <label>Price</label>
-
-            <Field
-              name="price"
-              type="number"
-              className="form-control"
-            />
-
-            <ErrorMessage
-              name="price"
-              component="div"
-              className="text-danger"
-            />
-
-            <br />
-
-            {/* Description */}
-
-            <label>Description</label>
-
-            <Field
-              as="textarea"
-              name="description"
-              className="form-control"
-            />
-
-            <ErrorMessage
-              name="description"
-              component="div"
-              className="text-danger"
-            />
-
-            <br />
-
-            {/* Image */}
-
-            <label>Food Image</label>
-
-            <input
-              type="file"
-              multiple
-              className="form-control"
-              accept="image/jpeg, image/jpg, image/png, image/webp"
-              onChange={(event) => {
-                setFieldValue(
-                  "files",
-                  Array.from(event.currentTarget.files)
-                );
-              }}
-            />
-
-
-            <ErrorMessage
-              name="files"
-              component="div"
-              className="text-danger"
-            />
-            <Row>
-              {values.files.map((file, index) => (
-                <Col md={3} key={index}>
-                  <img
-                  key={index}
-                  src={URL.createObjectURL(file)}
-                  alt="preview"
-                  width="120"
-                  height="120"
-                  style={{ objectFit: "cover", borderRadius: "5px" }}
-                />
-                </Col>  
-              ))}
-            </Row>
-            <br />
-
-            <button
-              type="submit"
-              className="btn btn-success"
-            >
-              Add Product
-            </button>
-
-=======
             {/* Food Type */}
 
             <label>Food Type</label>
@@ -393,7 +211,6 @@ export default function AddProduct() {
             <button type="submit" className="btn btn-success">
               Add Product
             </button>
->>>>>>> Stashed changes
           </Form>
         )}
       </Formik>
