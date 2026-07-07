809f8da93769ab5d8c8b9ec3da953f7494f64f3e
aanupam2004-source <anupam232322@arkajainuniversity.ac.in>
2026-07-07 22:25:05 +0530

    correct register.js

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -3,11 +3,15 @@ import { Col, Container, Row, Offcanvas, Button } from "react-bootstrap";
 import { useFormik } from "formik";
 import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
+import axios from "axios";
 
 const LoginSchema = Yup.object().shape({
-   email: Yup.string()
-  .matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, "Enter a valid email address")
-  .required("Email required"),
+  email: Yup.string()
+    .matches(
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+      "Enter a valid email address",
+    )
+    .required("Email required"),
   password: Yup.string()
     .required("Password is required")
     .min(5, "Minimum 5 characters")
@@ -27,17 +31,17 @@ const SignupSchema = Yup.object().shape({
     .max(50, "lastname must not exceed 50 characters")
     .matches(/^[A-Za-z .]+$/, "Name can only contain letters")
     .required("lastname is Required"),
-    
+
   mobileNumber: Yup.string()
-    .matches(
-      /^[6-9]\d{9}$/,
-      "enter valid 10 digit numbers",
-    )
+    .matches(/^[6-9]\d{9}$/, "enter valid 10 digit numbers")
     // .required("only digits are required")
-    .required("Mobile number is required"),                               
+    .required("Mobile number is required"),
   email: Yup.string()
-  .matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, "Enter a valid email address")
-  .required("Email required"),
+    .matches(
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+      "Enter a valid email address",
+    )
+    .required("Email required"),
   password: Yup.string()
     .required("Password is required")
     .min(5, "Minimum 5 characters")
@@ -47,7 +51,7 @@ const SignupSchema = Yup.object().shape({
 });
 
 const Login = () => {
-  const [show, setShow] = useState(false);
+  const [show, setShow] = useState(true);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
@@ -67,74 +71,71 @@ const Login = () => {
                 }}
                 validationSchema={SignupSchema}
                 onSubmit={(values) => {
-                  console.log(values);
+                  const data = {
+                    username: values.mobileNumber,
+                    firstName: values.firstName,
+                    lastName: values.lastName,
+                    mobileNumber: values.mobileNumber,
+                    email: values.email,
+                    password: values.password,
+                  };
+
+                  console.log(data);
+                  axios
+                    .post("http://localhost:8090/api/auth/signup", data)
+                    .then((response) => {
+                      console.log("user sucessfully Registered");
+                    })
+                    .catch((error) => {
+                      console.log("user reistered failed");
+                    });
                 }}
               >
                 {({ errors, touched }) => (
                   <Form>
-                    <Row>
-                      <Col>
-                        <label htmlFor="firstName">First Name</label>
-                      </Col>
-                      <Col>
+                   
+                        <label htmlFor="firstName">First Name:</label>
+                     
                         <Field name="firstName" />
-                        {
-                        errors.firstName && touched.firstName ? (
+                        {errors.firstName && touched.firstName ? (
                           <div>{errors.firstName}</div>
                         ) : null}
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col>
-                        <label htmlFor="lastName">Last Name</label>
-                      </Col>
-                      <Col>
+                    
+                        <label htmlFor="lastName">Last Name:</label>
+                    
                         <Field name="lastName" />
                         {errors.lastName && touched.lastName ? (
                           <div>{errors.lastName}</div>
                         ) : null}
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col>
-                        <label htmlFor="mobileNumber">Mobile</label>
-                      </Col>
-                      <Col>
+                    
+                    
+                        <label htmlFor="mobileNumber">Mobile:</label>
+                    
                         <Field name="mobileNumber" />
                         {errors.mobileNumber && touched.mobileNumber ? (
                           <div>{errors.mobileNumber}</div>
                         ) : null}
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col>
-                        <label htmlFor="email">Email</label>
-                      </Col>
-                      <Col>
+                      
+                        <label htmlFor="email">Email:</label>
+                     
                         <Field name="email" type="email" />
                         {errors.email && touched.email ? (
                           <div>{errors.email}</div>
                         ) : null}
-                      </Col>
-                    </Row>
+                    
 
-                    <Row>
-                      <Col>
-                        <label htmlFor="password">Password</label>
-                      </Col>
-                      <Col>
+                   
+                        <label htmlFor="password">Password:</label>
+                     
                         <Field name="password" />
                         {errors.password && touched.password ? (
                           <div>{errors.password}</div>
                         ) : null}
-                      </Col>
-                    </Row>
+                     
 
-                    <Row>
-                      <Col>
-                        <button type="submit">Submit</button>
-                      </Col>
-                    </Row>
+                    
+                        <button type="submit">Register</button>
+                    
                   </Form>
                 )}
               </Formik>
@@ -194,7 +195,7 @@ const Login = () => {
                   </Form>
                 )}
               </Formik>
-              <button  className="register_btn"  onClick={handleShow}>
+              <button className="register_btn" onClick={handleShow}>
                 If you are not registerd then click here
               </button>
             </div>
